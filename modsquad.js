4e3e2fd56aae2e432d51135e4fece7866a39e8bd
Meg Cruzen <megcruzen@gmail.com>
2018-12-23 16:20:19 -0500

    Added ModSquad and Simon Says exercises

--- modsquad.js
+++ modsquad.js
@@ -17,3 +17,22 @@
 
 document.querySelector(".show-info").innerHTML = HTMLRepresentation
 */
+
+
+{
+    const ModSquad = {
+        "members": ["Pete Cochran", "Linc Hayes", "Julie Barnes", "Capt. Adam Greer", "Chief Barney Metcalf"],
+        "series": {
+            "start": "1968",
+            "end": "1973"
+        },
+    }
+    // console.log("ModSquad Members:", ModSquad.members);
+    let infoContainer = document.querySelector(".show-info");
+    let HTMLTitle = `<h1>The Mod Squad</h1>`;
+    infoContainer.innerHTML = HTMLTitle;
+    ModSquad.members.forEach(member => {
+        let HTMLRepresentation = `<div>${member}</div>`
+        infoContainer.innerHTML += HTMLRepresentation;
+    });
+}
